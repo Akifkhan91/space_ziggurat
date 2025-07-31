39826acff3df90fef464ceddb4152cb2c74e31c0
space_ziggurat <35231516+Akifkhan91@users.noreply.github.com>
2025-07-31 17:37:56 -0400

    Delete projected_gradient_descent_with_backtracking_line_search.jl

--- projected_gradient_descent_with_backtracking_line_search.jl
+++
@@ -1,7 +0,0 @@
-function projected_gradient_descent_with_backtracking_line_search(initial_and_boundary_conditions,data)
-    initial_step_size = 1.0
-    shrink_factor = 0.5
-    sufficient_decrease_factor = 0.5
-
-    
-end
